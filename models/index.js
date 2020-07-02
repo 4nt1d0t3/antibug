56815ff12db8396e9e23c47e2f419f13db7e111c
Antti Hyppola <antti.hyppola@hotmail.com>
2020-07-02 17:24:00 +0100

    add jawsDB connection... hopefully

--- models/index.js
+++ models/index.js
@@ -7,8 +7,8 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL, config);
 } else {
   sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, config);
 }
